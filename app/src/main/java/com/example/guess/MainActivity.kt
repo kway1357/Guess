016f6243b42a01f1b3367f84c52b123ff8da6fc2
yiming <zxtalj@mail.kway.com.tw>
2021-04-16 09:34:56 +0800

    fumction design, toast, dailog

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatViewInflater
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    val secreNumber = SecreNumber()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity","secret:" + secreNumber.secret)
+    }
+    fun check(view:View){
+        val n:Int = number.text.toString().toInt()
+        println("number: $n")
+        Log.d("MainActivity", "number:" + n)
+
+        val diff:Int = secreNumber.validate(n)
+        var message = "Yes, you got it"
+
+        if(secreNumber.validate(n) < 0){
+           // Toast.makeText(this, "Bigger", Toast.LENGTH_LONG).show()
+            message = "Bigger"
+        }else if(diff > 0){
+           // Toast.makeText(this, "Smaller", Toast.LENGTH_LONG).show()
+            message = "Smaller"
+        }
+        // Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
+
     }
 }
